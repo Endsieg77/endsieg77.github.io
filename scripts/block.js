fd2d3a8b70c0ece632a10709c1e3db08d7c9f77b
Endsieg77 <1054296510@qq.com>
2021-01-12 14:21:36 +0800

    Site updated: 2021-01-12 14:21:36

---
+++ scripts/block.js
@@ -0,0 +1,9 @@
+function ShowBox() {
+    document.getElementById('_Util').style.display='block';
+    document.getElementById('_Container').style.display='block';
+}
+
+function CloseBox() {
+    document.getElementById('_Util').style.display='none';
+    document.getElementById('_Container').style.display='none';
+}
