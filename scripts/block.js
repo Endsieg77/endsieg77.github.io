7327e9b2732870ef2779926720558faaca79f3ed
Endsieg77 <1054296510@qq.com>
2021-01-17 17:28:47 +0800

    Site updated: 2021-01-17 17:28:47

--- scripts/block.js
+++
@@ -1,9 +0,0 @@
-function ShowBox() {
-    document.getElementById('_Util').style.display='block';
-    document.getElementById('_Container').style.display='block';
-}
-
-function CloseBox() {
-    document.getElementById('_Util').style.display='none';
-    document.getElementById('_Container').style.display='none';
-}
